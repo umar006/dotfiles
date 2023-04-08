9384f7b7b3ab3a475f1be1c836b0ceb60f2aeb01
umar006 <umaralfaruq597@gmail.com>
2023-04-08 21:55:08 +0700

    Remove mode status

--- .config/nvim/after/plugin/statusline.lua
+++ .config/nvim/after/plugin/statusline.lua
@@ -1,17 +1,19 @@
-require('lualine').setup {
-    options = {
-        icons_enabled = false,
-        theme = "auto",
-        component_separators = '|',
-        section_separators = '',
-    },
-    sections = {
-        lualine_c = {
-            {
-                'filename',
-                file_status = true,
-                path = 1,
-            },
-        }
-    }
-}
+require("lualine").setup({
+	options = {
+		icons_enabled = false,
+		theme = "auto",
+		component_separators = "|",
+		section_separators = "",
+	},
+	sections = {
+		lualine_a = {},
+		lualine_c = {
+			{
+				"filename",
+				file_status = true,
+				path = 1,
+			},
+		},
+		lualine_x = {},
+	},
+})
