6db3038bd0f61c200cf258a693a58027e3b51a08
umar006 <umaralfaruq597@gmail.com>
2023-04-08 21:50:59 +0700

    Modify colorscheme function

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,33 +1,7 @@
--- require("catppuccin").setup({
--- 	flavour = "mocha",
--- 	-- color_overrides = {
--- 	-- 	mocha = {
--- 	-- 		base = "#000000",
--- 	-- 	},
--- 	-- },
--- })
-
--- require("gruvbox").setup({
--- 	invert_selection = true,
--- 	contrast = "hard",
--- 	-- transparent_mode = true,
--- })
-
--- vim.cmd.colorscheme("gruvbox")
-
--- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
--- vim.api.nvim_set_hl(0, "VertSplit", { fg = "#7aa2f7" })
-
 function ColorMyPencils(color, transparent)
 	require("catppuccin").setup({
 		flavour = "mocha",
 		transparent_background = transparent or false,
-		-- color_overrides = {
-		-- 	mocha = {
-		-- 		base = "#000000",
-		-- 	},
-		-- },
 	})
 
 	require("gruvbox").setup({
@@ -36,13 +10,21 @@ function ColorMyPencils(color, transparent)
 		transparent_mode = transparent or false,
 	})
 
+	require("rose-pine").setup({
+		dark_variant = "moon",
+		disable_background = transparent or false,
+		disable_float_background = transparent or false,
+		highlight_groups = {
+			Pmenu = { bg = "surface" },
+			LspSignatureActiveParameter = { bg = "pine", fg = "text" },
+			CursorLineNr = { fg = "rose" },
+		},
+	})
+
 	color = color or "catppuccin-mocha"
 	vim.cmd.colorscheme(color)
 
-	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-	-- vim.api.nvim_set_hl(0, "LineNr", { fg = "#7aa2f7" })
 	vim.api.nvim_set_hl(0, "VertSplit", { fg = "#7aa2f7" })
 end
 
-ColorMyPencils("gruvbox", false)
+ColorMyPencils("rose-pine", false)
