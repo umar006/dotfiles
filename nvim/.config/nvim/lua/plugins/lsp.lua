2c0a4d080c63d6fe7bc6e33c2ae50308436e1e0c
umar006 <umaralfaruq597@gmail.com>
2023-11-26 16:19:48 +0700

    Refactor setup lsp for lua

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -110,12 +110,7 @@ return {
         local servers = {
             gopls = {},
             tsserver = {},
-            lua_ls = {
-                Lua = {
-                    workspace = { checkThirdParty = false },
-                    telemetry = { enable = false },
-                },
-            },
+            lua_ls = {},
             eslint = {},
         }
 
@@ -172,6 +167,36 @@ return {
                             staticcheck = true,
                         },
                     },
+                })
+            end,
+
+            ["lua_ls"] = function()
+                local lspconfig = require("lspconfig")
+                lspconfig.lua_ls.setup({
+                    on_init = function(client)
+                        local path = client.workspace_folders[1].name
+                        if
+                            not vim.loop.fs_stat(path .. "/.luarc.json")
+                            and not vim.loop.fs_stat(path .. "/.luarc.jsonc")
+                        then
+                            client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+                                Lua = {
+                                    runtime = {
+                                        version = "LuaJIT",
+                                    },
+                                    workspace = {
+                                        checkThirdParty = false,
+                                        library = {
+                                            vim.env.VIMRUNTIME,
+                                        },
+                                    },
+                                },
+                            })
+
+                            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+                        end
+                        return true
+                    end,
                 })
             end,
         })
