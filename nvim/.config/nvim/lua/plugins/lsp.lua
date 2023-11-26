17b65724b065cc32b76567f8f0ff755ae42bd2e3
umar006 <umaralfaruq597@gmail.com>
2023-11-26 14:56:04 +0700

    Create cmd for tsserver to organize imports

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -148,6 +148,28 @@ return {
                     settings = servers[server_name],
                 })
             end,
+
+            ["tsserver"] = function()
+                local function organize_imports()
+                    local params = {
+                        command = "_typescript.organizeImports",
+                        arguments = { vim.api.nvim_buf_get_name(0) },
+                        title = "",
+                    }
+                    vim.lsp.buf.execute_command(params)
+                end
+
+                require("lspconfig").tsserver.setup({
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                    commands = {
+                        OrganizeImports = {
+                            organize_imports,
+                            description = "Organize Imports",
+                        },
+                    },
+                })
+            end,
         })
     end,
 }
