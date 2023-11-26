4e5c4e6eaca35fe68787b20397b0986d2ba6a7d7
umar006 <umaralfaruq597@gmail.com>
2023-11-26 13:42:18 +0700

    Use telescope builtin for lsp implementations

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -87,7 +87,7 @@ return {
 
             nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
             nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-            nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+            nmap("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
             nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
             nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
             nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
