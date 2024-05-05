d21d165fefadf8a479e352a332f3512e22f295c6
umar006 <umaralfaruq597@gmail.com>
2024-05-05 19:31:59 +0700

    feat: add nonels formatter

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -1,8 +1,41 @@
 local slow_format_filetypes = {}
+local nonels = {
+    "nvimtools/none-ls.nvim",
+    -- event = { "BufWritePre" },
+    config = function()
+        local null_ls = require("null-ls")
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier,
+            },
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            if vim.g.disable_autoformat then
+                                return
+                            end
+                            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                            -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                            vim.lsp.buf.format({ bufnr = bufnr })
+                        end,
+                    })
+                end
+            end,
+        })
+    end,
+}
+
 return {
+    -- nonels,
     {
         "stevearc/conform.nvim",
-        event = { "VeryLazy", "BufWritePre" },
+        event = { "BufWritePre" },
         cmd = { "ConformInfo" },
         opts = {
             formatters = {
@@ -29,6 +62,7 @@ return {
                     local util = require("conform.util")
 
                     return {
+                        prepend_args = { "--cache" },
                         require_cwd = true,
                         cwd = util.root_file({
                             -- https://eslint.org/docs/latest/user-guide/configuring/configuration-files-new
@@ -46,40 +80,23 @@ return {
             },
             formatters_by_ft = {
                 lua = { "stylua" },
-                html = { "prettierd" },
-                css = { "prettierd" },
-                javascript = { { "biome", "eslint_d", "prettierd" } },
-                javascriptreact = { { "biome", "eslint_d", "prettierd" } },
-                typescript = { { "biome", "eslint_d", "prettierd" } },
-                typescriptreact = { { "biome", "eslint_d", "prettierd" } },
+                html = { "prettier" },
+                css = { "prettier" },
+                javascript = { "prettier", "eslint_d" },
+                javascriptreact = { "prettierd", "eslint_d" },
+                typescript = { "prettierd" },
+                typescriptreact = { "prettierd" },
                 go = { "goimports", "gofumpt" },
                 json = { "jq" },
                 sh = { "shfmt" },
                 yaml = { "prettierd" },
-                ["*"] = { "trim_whitespace", "trim_newlines" },
-                ["_"] = { "trim_whitespace", "trim_newlines" },
             },
             format_on_save = function(bufnr)
                 if vim.g.disable_autoformat then
                     return
                 end
 
-                if slow_format_filetypes[vim.bo[bufnr].filetype] then
-                    return
-                end
-                local function on_format(err)
-                    if err and err:match("timeout$") then
-                        slow_format_filetypes[vim.bo[bufnr].filetype] = true
-                    end
-                end
-
-                return { timeout_ms = 500, lsp_fallback = true }, on_format
-            end,
-            format_after_save = function(bufnr)
-                if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-                    return
-                end
-                return { lsp_fallback = true }
+                return { timeout_ms = 10000, lsp_fallback = true }
             end,
             log_level = vim.log.levels.DEBUG,
         },
