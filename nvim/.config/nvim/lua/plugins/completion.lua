6ca0181a1e6675daf050277f5eb736fd87160d65
umar006 <umaralfaruq597@gmail.com>
2024-05-05 19:31:38 +0700

    refactor: snippet configuration

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -1,29 +1,30 @@
 return {
     {
-        "L3MON4D3/LuaSnip",
-        event = "VeryLazy",
-        dependencies = {
-            "rafamadriz/friendly-snippets",
-            config = function()
-                require("luasnip.loaders.from_vscode").lazy_load()
-                -- vim.api.nvim_create_autocmd("InsertLeave", {
-                --   callback = function()
-                --     if
-                --       require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
-                --       and not require("luasnip").session.jump_active
-                --     then
-                --       require("luasnip").unlink_current()
-                --     end
-                --   end,
-                -- })
-            end,
-        },
-        opts = { history = true, updateevents = "TextChanged,TextChangedI" },
-    },
-    {
         "hrsh7th/nvim-cmp",
-        event = { "VeryLazy", "InsertEnter" },
+        event = { "InsertEnter" },
         dependencies = {
+            {
+                "L3MON4D3/LuaSnip",
+                dependencies = {
+                    "rafamadriz/friendly-snippets",
+                    opts = { history = true, updateevents = "TextChanged,TextChangedI" },
+                    config = function()
+                        require("luasnip.loaders.from_vscode").lazy_load()
+
+                        vim.api.nvim_create_autocmd("InsertLeave", {
+                            callback = function()
+                                if
+                                    require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+                                    and not require("luasnip").session.jump_active
+                                then
+                                    require("luasnip").unlink_current()
+                                end
+                            end,
+                        })
+                    end,
+                },
+            },
+
             "onsails/lspkind.nvim",
 
             -- cmp sources plugins
