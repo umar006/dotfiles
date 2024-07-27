9b5165a5591c0c740c26603bc2ecc22c69354312
umar006 <umaralfaruq597@gmail.com>
2024-07-27 11:02:51 +0700

    remove: unnecessary code

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -11,57 +11,19 @@ return {
             { "williamboman/mason-lspconfig.nvim", opts = {} },
         },
         config = function()
-            local document_hightlight = function(client, bufnr)
-                local status_ok, highlight_supported = pcall(function()
-                    return client.supports_method("textDocument/documentHighlight")
-                end)
-                if not status_ok or not highlight_supported then
-                    return
-                end
-                local group = "lsp_document_highlight"
-                local hl_events = { "CursorHold", "CursorHoldI" }
-
-                local ok, hl_autocmds = pcall(vim.api.nvim_get_autocmds, {
-                    group = group,
-                    buffer = bufnr,
-                    event = hl_events,
-                })
-
-                if ok and #hl_autocmds > 0 then
-                    return
-                end
-
-                vim.api.nvim_create_augroup(group, { clear = false })
-                vim.api.nvim_create_autocmd(hl_events, {
-                    group = group,
-                    buffer = bufnr,
-                    callback = vim.lsp.buf.document_highlight,
-                })
-                vim.api.nvim_create_autocmd("CursorMoved", {
-                    group = group,
-                    buffer = bufnr,
-                    callback = vim.lsp.buf.clear_references,
-                })
-            end
-
-            local on_attach = function(client, bufnr)
-                -- document_hightlight(client, bufnr)
-            end
+            vim.diagnostic.config({
+                virtual_text = true,
+            })
 
             local capabilities = vim.lsp.protocol.make_client_capabilities()
             capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
             local mason_lspconfig = require("mason-lspconfig")
 
-            mason_lspconfig.setup({
-                ensure_installed = { "gopls", "tsserver", "lua_ls", "eslint", "cssls", "html" },
-            })
-
             mason_lspconfig.setup_handlers({
                 function(server_name)
                     require("lspconfig")[server_name].setup({
                         capabilities = capabilities,
-                        on_attach = on_attach,
                     })
                 end,
 
@@ -78,7 +40,6 @@ return {
                     local lspconfig = require("lspconfig")
                     lspconfig.tsserver.setup({
                         capabilities = capabilities,
-                        on_attach = on_attach,
                         commands = {
                             OrganizeImports = {
                                 organize_imports,
@@ -92,7 +53,6 @@ return {
                     local lspconfig = require("lspconfig")
                     lspconfig.gopls.setup({
                         capabilities = capabilities,
-                        on_attach = on_attach,
                         cmd = { "gopls", "serve" },
                         settings = {
                             gopls = {
@@ -110,34 +70,33 @@ return {
                     local lspconfig = require("lspconfig")
                     lspconfig.lua_ls.setup({
                         capabilities = capabilities,
-                        on_attach = on_attach,
                         on_init = function(client)
                             local path = client.workspace_folders[1].name
                             if
-                                not vim.loop.fs_stat(path .. "/.luarc.json")
-                                and not vim.loop.fs_stat(path .. "/.luarc.jsonc")
+                                vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc")
                             then
-                                client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
-                                    Lua = {
-                                        diagnostics = {
-                                            globals = { "vim" },
-                                        },
-                                        runtime = {
-                                            version = "LuaJIT",
-                                        },
-                                        workspace = {
-                                            checkThirdParty = false,
-                                            library = {
-                                                vim.env.VIMRUNTIME,
-                                            },
-                                        },
-                                    },
-                                })
-
-                                client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+                                return
                             end
-                            return true
+
+                            client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+                                runtime = {
+                                    -- Tell the language server which version of Lua you're using
+                                    -- (most likely LuaJIT in the case of Neovim)
+                                    version = "LuaJIT",
+                                },
+                                -- Make the server aware of Neovim runtime files
+                                workspace = {
+                                    checkThirdParty = false,
+                                    library = {
+                                        vim.env.VIMRUNTIME,
+                                        "${3rd}/luv/library",
+                                    },
+                                },
+                            })
                         end,
+                        settings = {
+                            Lua = {},
+                        },
                     })
                 end,
             })
